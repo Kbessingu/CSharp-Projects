89a2baed4c87a176d9261f36d05817524c3970dd
ByteNavi <147370810+Kbessingu@users.noreply.github.com>
2025-08-06 11:34:39 +0200

    Delete CLI-Apps/DiceApp/Basic-Dice.cs
    was a test

--- CLI-Apps/DiceApp/Basic-Dice.cs
+++
@@ -1,9 +0,0 @@
-﻿/*
-Simple command line Dice app
-Using Random class, calling methods, string interpolation
-*/
-
-Random dice = new Random();
-int roll = dice.Next(1, 7);
-Console.WriteLine($"You Rolled a: {roll}");
-
