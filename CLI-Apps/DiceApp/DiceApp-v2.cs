a737cb0afb5f3b873037bb2cf96c78391286cce0
ByteNavi <147370810+Kbessingu@users.noreply.github.com>
2025-08-06 13:41:48 +0200

    Create DiceApp-v2.cs

---
+++ CLI-Apps/DiceApp/DiceApp-v2.cs
@@ -0,0 +1,13 @@
+/*
+Command line Dice app with 3 rolls
+Using overloaded version of Random.Next() method
+*/
+
+Random dice = new Random();
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
