9c3269059fdaab36dc4142ec0905b9413fc77ae1
ByteNavi <147370810+Kbessingu@users.noreply.github.com>
2025-08-06 18:51:08 +0200

    Update Calculator-v1.cs

--- CLI-Apps/Calculator/Calculator-v1.cs
+++ CLI-Apps/Calculator/Calculator-v1.cs
@@ -7,11 +7,11 @@ double num1 = 10;
 double num2 = 5;
 
 double addition = num1 + num2;
-double substaction = num1 - num2;
+double substraction = num1 - num2;
 double multiplication = num1 * num2;
 double division = num1 / num2;
 
 Console.WriteLine($"{num1} + {num2} = {addition}");
-Console.WriteLine($"{num1} - {num2} = {substaction}");
+Console.WriteLine($"{num1} - {num2} = {substraction}");
 Console.WriteLine($"{num1} * {num2} = {multiplication}");
 Console.WriteLine($"{num1} / {num2} = {division}");
