9177232d290554ffc9564de5d4ba550e18190739
ByteNavi <147370810+Kbessingu@users.noreply.github.com>
2025-08-06 11:33:21 +0200

    Create DiceAbb-v1.cs

---
+++ CLI-Apps/DiceApp/DiceAbb-v1.cs
@@ -0,0 +1,8 @@
+/*
+Simple command line Dice app
+Using Random class, calling methods, string interpolation
+*/
+
+Random dice = new Random();
+int roll = dice.Next(1, 7);
+Console.WriteLine($"You Rolled a: {roll}");
