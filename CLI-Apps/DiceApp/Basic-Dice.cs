16cf688dc6435b7c917e9a86194fa525ba47e60b
ByteNavi <147370810+Kbessingu@users.noreply.github.com>
2025-08-06 11:33:53 +0200

    Add files via upload

---
+++ CLI-Apps/DiceApp/Basic-Dice.cs
@@ -0,0 +1,9 @@
+﻿/*
+Simple command line Dice app
+Using Random class, calling methods, string interpolation
+*/
+
+Random dice = new Random();
+int roll = dice.Next(1, 7);
+Console.WriteLine($"You Rolled a: {roll}");
+
